4357da60006878e285aa52bfa9e4465aaa784d70
David Schleef <ds@schleef.org>
2009-03-28 00:00:00 -0700

    add a compiler tool

--- tools/orcc.c
+++ tools/orcc.c
@@ -1,10 +1,71 @@
 
+#include <orc/orc.h>
+#include <orc-test/orctest.h>
+#include <orc/orcparse.h>
+
 #include <stdio.h>
+#include <stdlib.h>
+
+static char * read_file (const char *filename);
 
 int
 main (int argc, char *argv[])
 {
+  char *code;
+  int n;
+  int i;
+  OrcProgram **programs;
+
+  orc_init ();
+  orc_test_init ();
+
+  code = read_file ("test.orc");
+
+  n = orc_parse (code, &programs);
+
+  for(i=0;i<n;i++){
+    printf("%s\n", programs[i]->name);
+    orc_test_gcc_compile (programs[i]);
+  }
 
   return 0;
+}
+
+
+static char *
+read_file (const char *filename)
+{
+  FILE *file = NULL;
+  char *contents = NULL;
+  long size;
+  int ret;
+
+  file = fopen (filename, "r");
+  if (file == NULL) return NULL;
+
+  ret = fseek (file, 0, SEEK_END);
+  if (ret < 0) goto bail;
+
+  size = ftell (file);
+  if (size < 0) goto bail;
+
+  ret = fseek (file, 0, SEEK_SET);
+  if (ret < 0) goto bail;
+
+  contents = malloc (size + 1);
+  if (contents == NULL) goto bail;
+
+  ret = fread (contents, size, 1, file);
+  if (ret < 0) goto bail;
+
+  contents[size] = 0;
+
+  return contents;
+bail:
+  /* something failed */
+  if (file) fclose (file);
+  if (contents) free (contents);
+
+  return NULL;
 }
 
