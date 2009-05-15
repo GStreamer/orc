d0f45d4ca853ec39d414e896d584d35650c3b26a
David Schleef <ds@schleef.org>
2009-05-14 22:19:02 -0700

    remove orc-stdint.h

--- orc/orc-stdint.h
+++
@@ -1,9 +0,0 @@
-#ifndef _ORC_ORC_ORC_STDINT_H
-#define _ORC_ORC_ORC_STDINT_H 1
-#ifndef _GENERATED_STDINT_H
-#define _GENERATED_STDINT_H "orc 0.3.14.1"
-/* generated using gnu compiler gcc (Ubuntu 4.3.2-1ubuntu12) 4.3.2 */
-#define _STDINT_HAVE_STDINT_H 1
-#include <stdint.h>
-#endif
-#endif
