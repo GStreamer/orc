3a7c3f1a7ef41b09598fdce7e848e7c4d3636648
David Schleef <ds@schleef.org>
2009-05-23 12:29:52 -0700

    neon: hacking

--- orc/neon.h
+++ orc/neon.h
@@ -8,10 +8,12 @@
 void orc_neon_loadb (OrcCompiler *compiler, int dest, int src1, int update, int is_aligned);
 void orc_neon_loadw (OrcCompiler *compiler, int dest, int src1, int update, int is_aligned);
 void orc_neon_loadl (OrcCompiler *compiler, int dest, int src1, int update, int is_aligned);
+void orc_neon_loadq (OrcCompiler *compiler, int dest, int src1, int update, int is_aligned);
 void orc_neon_neg (OrcCompiler *compiler, int dest);
 void orc_neon_storeb (OrcCompiler *compiler, int dest, int update, int src1, int is_aligned);
 void orc_neon_storew (OrcCompiler *compiler, int dest, int update, int src1, int is_aligned);
 void orc_neon_storel (OrcCompiler *compiler, int dest, int update, int src1, int is_aligned);
+void orc_neon_storeq (OrcCompiler *compiler, int dest, int update, int src1, int is_aligned);
 void orc_neon_emit_loadib (OrcCompiler *p, int reg, int value);
 void orc_neon_emit_loadiw (OrcCompiler *p, int reg, int value);
 void orc_neon_emit_loadil (OrcCompiler *p, int reg, int value);
