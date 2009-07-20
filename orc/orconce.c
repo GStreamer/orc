38dd192c5006bd66ef12a0a18c9182ffc68a94f9
David Schleef <ds@schleef.org>
2009-07-20 16:10:21 -0700

    once: add win32 threading code

--- orc/orconce.c
+++ orc/orconce.c
@@ -5,11 +5,12 @@
 #endif
 
 #include <orc/orconce.h>
+
+#if defined(HAVE_THREAD_PTHREAD)
+
 #include <pthread.h>
 
-
-pthread_mutex_t once_mutex = PTHREAD_MUTEX_INITIALIZER;
-
+static pthread_mutex_t once_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void
 orc_once_mutex_lock (void)
@@ -22,4 +23,37 @@ orc_mutex_unlock (OrcMutex *mutex)
 {
   pthread_mutex_unlock (&once_mutex);
 }
+
+#elif defined(HAVE_THREAD_WIN32)
+
+#include <windows.h>
+
+static CRITICAL_SECTION once_mutex;
+
+void
+orc_once_mutex_lock (void)
+{
+  EnterCriticalSection (&once_mutex);
+}
+
+void
+orc_mutex_unlock (OrcMutex *mutex)
+{
+  LeaveCriticalSection (&once_mutex);
+}
+
+#else
+
+void
+orc_once_mutex_lock (void)
+{
+}
+
+void
+orc_mutex_unlock (OrcMutex *mutex)
+{
+}
+
+#endif
+
 
