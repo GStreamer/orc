80b9a88663947addb6507a122a84c56e020bfdb6
David Schleef <ds@schleef.org>
2010-06-14 16:20:41 -0700

    tests: Add ABI check for code_exec

---
+++ testsuite/abi.c
@@ -0,0 +1,33 @@
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <orc/orc.h>
+
+#include <stdio.h>
+
+
+int main (int argc, char *argv[])
+{
+  int offset;
+  int expected_offset;
+  int error = 0;
+
+  offset = ((int) ((unsigned char *) &((OrcProgram*) 0)->code_exec));
+
+  if (sizeof(void *) == 4) {
+    expected_offset = 8360;
+  } else {
+    expected_offset = 9688;
+  }
+
+  if (offset != expected_offset) {
+    printf("ABI bug: OrcProgram->code_exec should be at offset %d instead of %d\n",
+        offset, expected_offset);
+    error = 1;
+  }
+
+  return error;
+}
+
