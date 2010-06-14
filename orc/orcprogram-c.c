GStreamer/orc

commit 5bfde389c78155e77d9f9bfc7dcfec86f31f7078

Add float parameters

orc/orcprogram-c.c
@@ -242,10 +242,16 @@ orc_compiler_c_assemble (OrcCompiler *compiler)
       case ORC_VAR_TYPE_PARAM:
         c_get_name (varname, compiler, i);
         if (!(compiler->target_flags & ORC_TARGET_C_NOEXEC)) {
-          ORC_ASM_CODE(compiler,"  const int %s = ex->params[%d];\n",
-              varname, i);
+          if (var->is_float_param) {
+            ORC_ASM_CODE(compiler,"  const float %s = ((orc_union32 *)(ex->params+%d))->f;\n",
+                varname, i);
+          } else {
+            ORC_ASM_CODE(compiler,"  const float %s = ((orc_union32 *)(ex->params+%d))->i;\n",
+                varname, i);
+          }
         } else {
-          ORC_ASM_CODE(compiler,"  const int %s = %s;\n",
+          ORC_ASM_CODE(compiler,"  const %s %s = %s;\n",
+              var->is_float_param ? "float" : "int",
               varname, varnames[i]);
         }
         break;

orc/orcprogram.c
@@ -374,6 +374,30 @@ orc_program_add_parameter (OrcProgram *program, int size, const char *name)
 }
 
 /**
+ * orc_program_add_parameter_float:
+ * @program: a pointer to an OrcProgram structure
+ * @size: size of data value
+ * @name: name of variable
+ *
+ * Creates a new variable representing a scalar parameter.
+ *
+ * Returns: the index of the new variable
+ */
+int
+orc_program_add_parameter_float (OrcProgram *program, int size, const char *name)
+{
+  int i = ORC_VAR_P1 + program->n_param_vars;
+
+  program->vars[i].vartype = ORC_VAR_TYPE_PARAM;
+  program->vars[i].is_float_param = TRUE;
+  program->vars[i].size = size;
+  program->vars[i].name = strdup(name);
+  program->n_param_vars++;
+
+  return i;
+}
+
+/**
  * orc_program_add_accumulator:
  * @program: a pointer to an OrcProgram structure
  * @size: size of data value

orc/orcprogram.h
@@ -207,6 +207,8 @@ struct _OrcVariable {
   int aligned_data;
   int sampling_type;
   int load_dest;
+  double float_value;
+  int is_float_param;
 };
 
 /**
@@ -534,6 +536,7 @@ int orc_program_add_source (OrcProgram *program, int size, const char *name);
 int orc_program_add_destination (OrcProgram *program, int size, const char *name);
 int orc_program_add_constant (OrcProgram *program, int size, int value, const char *name);
 int orc_program_add_parameter (OrcProgram *program, int size, const char *name);
+int orc_program_add_parameter_float (OrcProgram *program, int size, const char *name);
 int orc_program_add_accumulator (OrcProgram *program, int size, const char *name);
 void orc_program_set_type_name (OrcProgram *program, int var, const char *type_name);
 void orc_program_set_sampling_type (OrcProgram *program, int var, int sampling_type);

orc/orcparse.c
@@ -159,6 +159,9 @@ orc_parse (const char *code, OrcProgram ***programs)
         int size = strtol (token[1], NULL, 0);
         int value = strtoul (token[3], NULL, 0);
         orc_program_add_constant (parser->program, size, value, token[2]);
+      } else if (strcmp (token[0], ".floatparam") == 0) {
+        int size = strtol (token[1], NULL, 0);
+        orc_program_add_parameter_float (parser->program, size, token[2]);
       } else {
         ORC_ERROR("ERROR: unknown directive: %s", token[0]);
       }

tools/orcc.c
@@ -409,7 +409,9 @@ output_prototype (OrcProgram *p, FILE *output)
     var = &p->vars[ORC_VAR_P1 + i];
     if (var->size) {
       if (need_comma) fprintf(output, ", ");
-      fprintf(output, "int %s", varnames[ORC_VAR_P1 + i]);
+      fprintf(output, "%s %s",
+          var->is_float_param ? "float" : "int",
+          varnames[ORC_VAR_P1 + i]);
       need_comma = TRUE;
     }
   }
@@ -558,7 +560,8 @@ output_code (OrcProgram *p, FILE *output)
   for(i=0;i<8;i++){
     var = &p->vars[ORC_VAR_P1 + i];
     if (var->size) {
-      fprintf(output, "      orc_program_add_parameter (p, %d, \"%s\");\n",
+      fprintf(output, "      orc_program_add_parameter%s (p, %d, \"%s\");\n",
+          var->is_float_param ? "_float" : "",
           var->size, varnames[ORC_VAR_P1 + i]);
     }
   }
@@ -629,8 +632,17 @@ output_code (OrcProgram *p, FILE *output)
   for(i=0;i<8;i++){
     var = &p->vars[ORC_VAR_P1 + i];
     if (var->size) {
-      fprintf(output, "  ex->params[%s] = %s;\n",
-          enumnames[ORC_VAR_P1 + i], varnames[ORC_VAR_P1 + i]);
+      if (var->is_float_param) {
+        fprintf(output, "  {\n");
+        fprintf(output, "    orc_union32 tmp;\n");
+        fprintf(output, "    tmp.f = %s;\n", varnames[ORC_VAR_P1 + i]);
+        fprintf(output, "    ex->params[%s] = tmp.i;\n",
+            enumnames[ORC_VAR_P1 + i]);
+        fprintf(output, "  }\n");
+      } else {
+        fprintf(output, "  ex->params[%s] = %s;\n",
+            enumnames[ORC_VAR_P1 + i], varnames[ORC_VAR_P1 + i]);
+      }
     }
   }
   fprintf(output, "\n");
@@ -712,7 +724,8 @@ output_code_test (OrcProgram *p, FILE *output)
   for(i=0;i<8;i++){
     var = &p->vars[ORC_VAR_P1 + i];
     if (var->size) {
-      fprintf(output, "    orc_program_add_parameter (p, %d, \"%s\");\n",
+      fprintf(output, "    orc_program_add_parameter%s (p, %d, \"%s\");\n",
+          var->is_float_param ? "_float" : "",
           var->size, varnames[ORC_VAR_P1 + i]);
     }
   }

commit cec29b2aa8b33a04b2a436ca326bbf0b0d6994ec

Fix ABI issues caused by 5bfde389c78155
Although OrcVariable and OrcProgram are completely private,
orcc generates code that digs deep into OrcProgram for code_exec.
We're requiring that generated code is ABI compatible, so that
means the offset of code_exec in OrcProgram is part of the ABI.

Oops.

orc/orcprogram-c.c
@@ -275,27 +275,9 @@ orc_compiler_c_assemble (OrcCompiler *compiler)
             char s1[20], s2[20];
             get_varname(s1, compiler, i);
             get_varname_stride(s2, compiler, i);
-            switch (var->sampling_type) {
-              case ORC_SAMPLE_REGULAR:
-                ORC_ASM_CODE(compiler,
-                    "    ptr%d = ORC_PTR_OFFSET(%s, %s * j);\n",
-                    i, s1, s2);
-                break;
-              case ORC_SAMPLE_TRANSPOSED:
-                ORC_ASM_CODE(compiler,
-                    "    ptr%d = ORC_PTR_OFFSET(%s, %d * j);\n",
-                    i, s1, var->size);
-                break;
-              case ORC_SAMPLE_NEAREST:
-              case ORC_SAMPLE_BILINEAR:
-              case ORC_SAMPLE_FOUR_TAP:
-                ORC_ASM_CODE(compiler,
-                    "    ptr%d = ORC_PTR_OFFSET(%s, %s * j);\n",
-                    i, s1, s2);
-                break;
-              default:
-                ORC_COMPILER_ERROR(compiler, "eeek");
-            }
+            ORC_ASM_CODE(compiler,
+                "    ptr%d = ORC_PTR_OFFSET(%s, %s * j);\n",
+                i, s1, s2);
           }
           break;
         case ORC_VAR_TYPE_DEST:

orc/orcprogram.c
@@ -430,7 +430,7 @@ void
 orc_program_set_sampling_type (OrcProgram *program, int var,
     int sampling_type)
 {
-  program->vars[var].sampling_type = sampling_type;
+  /* This doesn't do anything yet */
 }
 
 /**

orc/orcprogram.h
@@ -205,10 +205,8 @@ struct _OrcVariable {
   int ptr_offset;
   int mask_alloc;
   int aligned_data;
-  int sampling_type;
-  int load_dest;
-  double float_value;
   int is_float_param;
+  int load_dest;
 };
 
 /**