71d638d473105204ac5ac8bfc6b9536d252847b2
David Schleef <ds@schleef.org>
2012-08-28 12:08:07 -0700

    install pre-commit hook

--- autogen.sh
+++ autogen.sh
@@ -4,6 +4,13 @@ if [ `whoami` = ds ] ; then
   confargs="$confargs --enable-gtk-doc"
 fi
 
+# install pre-commit hook for doing clean commits
+if test ! \( -x .git/hooks/pre-commit -a -L .git/hooks/pre-commit \);
+then
+    rm -f .git/hooks/pre-commit
+    ln -s ../../misc/pre-commit.hook .git/hooks/pre-commit
+fi
+
 autoreconf -i -f &&
 ./configure --disable-static $confargs $@
 
