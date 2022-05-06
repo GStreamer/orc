GStreamer/orc

commit bb5fcb310ab4334f4228d562f3cc8ee93b1067d4

orccodemem: Report errno during failures to create mmap codemap.

orc/orccodemem.c
@@ -14,6 +14,7 @@
 
 #ifdef HAVE_CODEMEM_MMAP
 #include <sys/mman.h>
+#include <sys/errno.h>
 #endif
 
 #ifdef HAVE_CODEMEM_VIRTUALALLOC
@@ -223,7 +224,7 @@ orc_code_region_allocate_codemem_dual_map (OrcCodeRegion *region,
   fd = mkstemp (filename);
   umask (mask);
   if (fd == -1) {
-    ORC_WARNING ("failed to create temp file");
+    ORC_WARNING ("failed to create temp file '%s'. err=%i", filename, errno);
     free (filename);
     return FALSE;
   }
@@ -241,14 +242,14 @@ orc_code_region_allocate_codemem_dual_map (OrcCodeRegion *region,
 
   region->exec_ptr = mmap (NULL, SIZE, exec_prot, MAP_SHARED, fd, 0);
   if (region->exec_ptr == MAP_FAILED) {
-    ORC_WARNING("failed to create exec map");
+    ORC_WARNING("failed to create exec map '%s'. err=%i", filename, errno);
     close (fd);
     return FALSE;
   }
   region->write_ptr = mmap (NULL, SIZE, PROT_READ|PROT_WRITE,
       MAP_SHARED, fd, 0);
   if (region->write_ptr == MAP_FAILED) {
-    ORC_WARNING ("failed to create write map");
+    ORC_WARNING ("failed to create write map '%s'. err=%i", filename, errno);
     munmap (region->exec_ptr, SIZE);
     close (fd);
     return FALSE;
@@ -269,7 +270,7 @@ orc_code_region_allocate_codemem_anon_map (OrcCodeRegion *region)
   region->exec_ptr = mmap (NULL, SIZE, PROT_READ|PROT_WRITE|PROT_EXEC,
       MAP_PRIVATE|MAP_ANONYMOUS, -1, 0);
   if (region->exec_ptr == MAP_FAILED) {
-    ORC_WARNING("failed to create write/exec map");
+    ORC_WARNING("failed to create write/exec map. err=%i", errno);
     return FALSE;
   }
   region->write_ptr = region->exec_ptr;

commit 03d9f144cb333057bb203cbecea3fc29bb336120

orccodemem: Fix use-after-free in error paths
In file included from gstreamer/subprojects/orc/orc/orc.h:7,
                 from gstreamer/subprojects/orc/orc/orcprogram.h:5,
                 from gstreamer/subprojects/orc/orc/orccodemem.c:30:
gstreamer/subprojects/orc/orc/orccodemem.c: In function ‘orc_code_region_allocate_codemem_dual_map.constprop’:
gstreamer/subprojects/orc/orc/orcdebug.h:138:3: warning: pointer ‘filename’ may be used after ‘free’ [-Wuse-after-free]
  138 |   orc_debug_print((level), __FILE__, ORC_FUNCTION, __LINE__, __VA_ARGS__); \
      |   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
gstreamer/subprojects/orc/orc/orcdebug.h:92:26: note: in expansion of macro ‘ORC_DEBUG_PRINT’
   92 | #define ORC_WARNING(...) ORC_DEBUG_PRINT(ORC_DEBUG_WARNING, __VA_ARGS__)
      |                          ^~~~~~~~~~~~~~~
gstreamer/subprojects/orc/orc/orccodemem.c:252:5: note: in expansion of macro ‘ORC_WARNING’
  252 |     ORC_WARNING ("failed to create write map '%s'. err=%i", filename, errno);
      |     ^~~~~~~~~~~
gstreamer/subprojects/orc/orc/orccodemem.c:234:3: note: call to ‘free’ here
  234 |   free (filename);
      |   ^~~~~~~~~~~~~~~
gstreamer/subprojects/orc/orc/orcdebug.h:138:3: warning: pointer ‘filename’ may be used after ‘free’ [-Wuse-after-free]
  138 |   orc_debug_print((level), __FILE__, ORC_FUNCTION, __LINE__, __VA_ARGS__); \
      |   ^~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
gstreamer/subprojects/orc/orc/orcdebug.h:92:26: note: in expansion of macro ‘ORC_DEBUG_PRINT’
   92 | #define ORC_WARNING(...) ORC_DEBUG_PRINT(ORC_DEBUG_WARNING, __VA_ARGS__)
      |                          ^~~~~~~~~~~~~~~
gstreamer/subprojects/orc/orc/orccodemem.c:245:5: note: in expansion of macro ‘ORC_WARNING’
  245 |     ORC_WARNING("failed to create exec map '%s'. err=%i", filename, errno);
      |     ^~~~~~~~~~~
gstreamer/subprojects/orc/orc/orccodemem.c:234:3: note: call to ‘free’ here
  234 |   free (filename);
      |   ^~~~~~~~~~~~~~~

Fixes: bb5fcb31

orc/orccodemem.c
@@ -231,12 +231,12 @@ orc_code_region_allocate_codemem_dual_map (OrcCodeRegion *region,
   if (force_unlink || !_orc_compiler_flag_debug) {
     unlink (filename);
   }
-  free (filename);
 
   n = ftruncate (fd, SIZE);
   if (n < 0) {
     ORC_WARNING("failed to expand file to size");
     close (fd);
+    free (filename);
     return FALSE;
   }
 
@@ -244,18 +244,21 @@ orc_code_region_allocate_codemem_dual_map (OrcCodeRegion *region,
   if (region->exec_ptr == MAP_FAILED) {
     ORC_WARNING("failed to create exec map '%s'. err=%i", filename, errno);
     close (fd);
+    free (filename);
     return FALSE;
   }
   region->write_ptr = mmap (NULL, SIZE, PROT_READ|PROT_WRITE,
       MAP_SHARED, fd, 0);
   if (region->write_ptr == MAP_FAILED) {
     ORC_WARNING ("failed to create write map '%s'. err=%i", filename, errno);
+    free (filename);
     munmap (region->exec_ptr, SIZE);
     close (fd);
     return FALSE;
   }
   region->size = SIZE;
 
+  free (filename);
   close (fd);
   return TRUE;
 }