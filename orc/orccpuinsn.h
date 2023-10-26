a8844a9ab66fa8367d660fde60bbcf0f06afbd75
L. E. Segovia <amy@centricular.com>
2023-10-26 19:29:43 -0300

    orc: Add a bit of documentation in the instruction structs
    Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/113>

--- orc/orccpuinsn.h
+++ orc/orccpuinsn.h
@@ -4,33 +4,55 @@
 
 #ifdef ORC_ENABLE_UNSTABLE_API
 
-typedef struct _OrcSysInsn OrcSysInsn;
-typedef struct _OrcSysOpcode OrcSysOpcode;
+#include <orc/orcutils.h>
 
 struct _OrcSysInsn {
+  // Orc instruction opcode
   int opcode;
+  // destination register
   int dest_reg;
+  // first source operand
   int src1_reg;
+  // second source operand
   int src2_reg;
 
+  // immediate parameter (if any)
   int immediate;
 
+  // memory address register
   int mem_reg;
+  // memory offset
   int memoffset;
+  // index register
   int indexreg;
+
+  // shift by (used in loads)
   int shift;
+  // data type size
   int size;
 };
 
 struct _OrcSysOpcode {
+  // Mnemonic
   char name[16];
+  // Type of instruction (source instruction set, operation type)
   int type;
+  // Unused
   int flags;
+  // (x86 only) Instruction set prefix (EVEX, VEX, 2-byte SSE...)
+  // See ss. 2.3.5 Intel® 64 and IA-32 Architectures Software Developer’s
+  // Manual
   orc_uint8 prefix;
+  // Opcode (may include the last byte of the prefix as an opcode extension)
+  // For x86, see ss. 2.1.2 Intel® 64 and IA-32 Architectures Software
+  // Developer’s Manual
   orc_uint32 code;
+  // Additional opcode (if any)
   int code2;
 };
 
+typedef struct _OrcSysInsn OrcSysInsn;
+typedef struct _OrcSysOpcode OrcSysOpcode;
 
 #define ORC_SYS_OPCODE_FLAG_FIXED (1<<0)
 
