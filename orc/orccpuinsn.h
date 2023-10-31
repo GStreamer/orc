03ef984c2ad5e6353cf70ed4a6e1847c5bc13b50
L. E. Segovia <amy@centricular.com>
2023-10-31 18:30:26 -0300

    cpuinsn: remove unused _OrcSysInsn struct
    Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/123>

--- orc/orccpuinsn.h
+++ orc/orccpuinsn.h
@@ -6,32 +6,6 @@
 
 #include <orc/orcutils.h>
 
-struct _OrcSysInsn {
-  // Orc instruction opcode
-  int opcode;
-  // destination register
-  int dest_reg;
-  // first source operand
-  int src1_reg;
-  // second source operand
-  int src2_reg;
-
-  // immediate parameter (if any)
-  int immediate;
-
-  // memory address register
-  int mem_reg;
-  // memory offset
-  int memoffset;
-  // index register
-  int indexreg;
-
-  // shift by (used in loads)
-  int shift;
-  // data type size
-  int size;
-};
-
 struct _OrcSysOpcode {
   // Mnemonic
   char name[16];
@@ -51,7 +25,6 @@ struct _OrcSysOpcode {
   int code2;
 };
 
-typedef struct _OrcSysInsn OrcSysInsn;
 typedef struct _OrcSysOpcode OrcSysOpcode;
 
 #define ORC_SYS_OPCODE_FLAG_FIXED (1<<0)
