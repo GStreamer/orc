15b940465362bd51f7fb3e721a57347e34d543ba
Tim-Philipp Müller <tim@centricular.com>
2024-08-08 12:59:49 +0100

    Revert "orccodemem: Fix attempt to allocate a region of size 0"
    This reverts commit 82fad254528a2f85662184cd001622b3e326cf6b.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/201>

--- orc/orccodemem.c
+++ orc/orccodemem.c
@@ -187,7 +187,7 @@ orc_code_allocate_codemem (OrcCode *code, int size)
   OrcCodeRegion *region;
   OrcCodeChunk *chunk;
   int aligned_size =
-      (MAX(1, size) + _orc_codemem_alignment) & (~_orc_codemem_alignment);
+      (size + _orc_codemem_alignment) & (~_orc_codemem_alignment);
 
   orc_global_mutex_lock ();
   chunk = orc_code_region_get_free_chunk (aligned_size);
