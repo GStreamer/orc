GStreamer/orc

commit 75523350973fc5d5a08344b835170788165381aa

orc: Apply warning-as-errors for implicit function declarations
Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/122>

meson.build
@@ -16,9 +16,16 @@ osxversion = curversion + 1
 
 add_project_arguments('-DHAVE_CONFIG_H', language : 'c')
 
-orc_inc = include_directories('.')
-
 cc = meson.get_compiler('c')
+
+# Error out if a function's missing
+add_project_arguments(cc.get_supported_arguments([
+  '-we4013',
+  '-Wno-implicit-int',
+  '-Wno-implicit-function-declaration',
+]), language : 'c')
+
+orc_inc = include_directories('.')
 
 cdata = configuration_data()      # config.h
 

commit 6ef3c07dec738c92aeaff3f213879c310babd4a0

orc: actually error out on implicit function declarations
-Wno-x disables the 'x' warning. Instead of just changing it to '-Wx',
change it to '-Werror=x', given implicit function declarations are never
acceptable.

This exposes a latent issue for macOS: Include <libkern/OSCacheControl.h>
where we need it for sys_dcache_flush and sys_icache_invalidate.

Fixes: 75523350973fc5d5a08344b835170788165381aa
Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/214>

meson.build
@@ -21,8 +21,8 @@ cc = meson.get_compiler('c')
 # Error out if a function's missing
 add_project_arguments(cc.get_supported_arguments([
   '-we4013',
-  '-Wno-implicit-int',
-  '-Wno-implicit-function-declaration',
+  '-Werror=implicit-int',
+  '-Werror=implicit-function-declaration',
 ]), language : 'c')
 
 orc_inc = include_directories('.')

orc/orcprogram-x86.c
@@ -11,6 +11,10 @@
 #include <orc/orcx86-private.h>
 #include <orc/orcinternal.h>
 
+#if defined(__APPLE__)
+#include  <libkern/OSCacheControl.h>
+#endif
+
 #if defined(_WIN32)
 #include <windows.h>
 #endif