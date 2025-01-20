617ba06edb05f6cbf3729c48bf2954d655626147
L. E. Segovia <amy@amyspark.me>
2025-01-19 22:55:48 -0300

    sse: Fix exchanged operands causing memory corruption on Windows
    This was introduced in https://gitlab.freedesktop.org/gstreamer/orc/-/commit/45c2be4f3a964148f2fa25ded3c37b4dedef08d8 .
    
    Didn't trigger any crash on CI, but the bug was carried over from MMX.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/216>

--- orc/orcprogram-sse.c
+++ orc/orcprogram-sse.c
@@ -318,7 +318,7 @@ sse_move_register_to_memoffset (OrcCompiler *compiler, int size, int reg1, int o
 static void
 sse_move_memoffset_to_register (OrcCompiler *compiler, int size, int offset, int reg1, int reg2, int is_aligned)
 {
-  orc_x86_emit_mov_memoffset_sse (compiler, size, reg1, offset, reg2, is_aligned);
+  orc_x86_emit_mov_memoffset_sse (compiler, size, offset, reg1, reg2, is_aligned);
 }
 
 static int
