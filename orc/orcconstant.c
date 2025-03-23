755b9b34ddcbdc8734531cf18173f459aa73c633
Jorge Zapata <jorgeluis.zapata@gmail.com>
2025-03-23 10:51:13 +0100

    constant: Improve the constant handling
    OrcConstant was an attempt to define constants programmatically but
    it is not used on every target and is limited to 128-bits. Extend
    such definition to store at most 512-bits and to splat 64-bits values.
    
    Add methods to resolve a constant and to compare them.
    
    Part-of: <https://gitlab.freedesktop.org/gstreamer/orc/-/merge_requests/233>

---
+++ orc/orcconstant.c
@@ -0,0 +1,91 @@
+#include "config.h"
+
+#include <string.h>
+#include <orc/orcdebug.h>
+#include <orc/orcconstant.h>
+
+orc_bool
+orc_constant_resolve (const OrcConstant *c, OrcConstant *r, int reg_size)
+{
+  /* The number of 64-bits values that fit in the register size */
+  int count_64 = reg_size / 8;
+  int i;
+
+  if (!count_64 || reg_size > sizeof (r->v)) {
+    ORC_ERROR ("Register size %d does not fit on a constant", reg_size);
+    return FALSE;
+  }
+  
+  r->type = ORC_CONST_FULL;
+  r->alloc_reg = 0;
+  r->label = 0;
+  r->use_count = 0;
+  switch (c->type) {
+    case ORC_CONST_ZERO:
+      memset (r->v, 0, sizeof (r->v));
+      break;
+    case ORC_CONST_FULL:
+      memcpy (r, c, sizeof (OrcConstant));
+      break;
+    case ORC_CONST_SPLAT_B:
+      memset (r->v, c->v[0].x8[0], sizeof (r->v)); 
+      break;
+    case ORC_CONST_SPLAT_W:
+      for (i = 0; i < count_64; i++) {
+        r->v[i].x4[0] = c->v[0].x4[0];
+        r->v[i].x4[1] = c->v[0].x4[0];
+        r->v[i].x4[2] = c->v[0].x4[0];
+        r->v[i].x4[3] = c->v[0].x4[0];
+      }
+      break;
+    case ORC_CONST_SPLAT_L:
+      for (i = 0; i < count_64; i++) {
+        r->v[i].x2[0] = c->v[0].x2[0];
+        r->v[i].x2[1] = c->v[0].x2[0];
+      }
+      break;
+    case ORC_CONST_SPLAT_Q:
+      for (i = 0; i < count_64; i++) {
+        r->v[i].i = c->v[0].i;
+      }
+      break;
+
+    case ORC_CONST_SPLAT_DQ:
+      for (i = 0; i < count_64; i+=2) {
+        r->v[i].i = c->v[0].i;
+        r->v[i+1].i = c->v[1].i;
+      }
+      break;
+
+    case ORC_CONST_SPLAT_QQ:
+      for (i = 0; i < count_64; i+=4) {
+        r->v[i].i = c->v[0].i;
+        r->v[i+1].i = c->v[1].i;
+        r->v[i+2].i = c->v[2].i;
+        r->v[i+3].i = c->v[3].i;
+      }
+      break;
+
+     default:
+       ORC_ERROR ("Impossible to resolve %d", c->type);
+       return FALSE;
+  }
+  return TRUE;
+}
+
+orc_bool
+orc_constant_is_equal (const OrcConstant *c1, const OrcConstant *c2,
+    int reg_size)
+{
+  OrcConstant r1, r2;
+
+  if (!orc_constant_resolve (c1, &r1, reg_size))
+    return FALSE;
+  if (!orc_constant_resolve (c2, &r2, reg_size))
+    return FALSE;
+
+  if (memcmp (&r1.v, &r2.v, sizeof (r1.v)))
+    return FALSE;
+  else
+    return TRUE;
+}
--- orc/orcconstant.h
+++ orc/orcconstant.h
@@ -7,14 +7,29 @@
 
 ORC_BEGIN_DECLS
 
+/**
+ * OrcConstantType:
+ *
+ * @ORC_CONST_ZERO      : A constant with value zero
+ * @ORC_CONST_SPLAT_B   : The byte, 1 byte (8 bits), will be repeated in all the register
+ * @ORC_CONST_SPLAT_W   : The word, 2 bytes (16 bits), will be repeated in all the register
+ * @ORC_CONST_SPLAT_L   : The doubleword, 4 bytes (32 bits), will be repeated in all the register
+ * @ORC_CONST_SPLAT_Q   : The quadword, 8 bytes (64 bits), will be repeated in all the register
+ * @ORC_CONST_SPLAT_DQ  : The double quadword, 16 bytes (128 bits), will be repeated in all the register
+ * @ORC_CONST_SPLAT_QQ  : The quadruple quadword, 32 bytes (512 bits), will be repeated in all the register
+ * @ORC_CONSTANT_FULL   : The constant will be stored as is
+ */
 
-enum {
+typedef enum _OrcConstantType {
   ORC_CONST_ZERO,
   ORC_CONST_SPLAT_B,
   ORC_CONST_SPLAT_W,
   ORC_CONST_SPLAT_L,
-  ORC_CONST_FULL
-};
+  ORC_CONST_FULL,
+  ORC_CONST_SPLAT_Q,
+  ORC_CONST_SPLAT_DQ,
+  ORC_CONST_SPLAT_QQ,
+} OrcConstantType;
 
 /**
  * OrcConstant:
@@ -23,17 +38,35 @@ enum {
  */
 struct _OrcConstant {
   /*< private >*/
-  int type;
-  int alloc_reg;
-  unsigned int value;
-  unsigned int full_value[4];
+  OrcConstantType type;
+  int alloc_reg; // This should be part of a new OrcCompilerConstant
+  unsigned int value; // unused
+  unsigned int full_value[4]; // unused
   int use_count;
-  int is_long;
+  int is_long; // unused
   int label;
+  orc_union64 v[8]; // A total of 8*64 = 512 bytes
 };
 
+#ifdef ORC_ENABLE_UNSTABLE_API
+ORC_API orc_bool orc_constant_resolve (const OrcConstant *c, OrcConstant *r,
+    int reg_size);
+ORC_API orc_bool orc_constant_is_equal (const OrcConstant *c1,
+    const OrcConstant *c2, int reg_size);
+
+/* The following helper macros set 1, 2, 4, 8, 16, 32 or 64 bytes of data
+ * in little-endian endianness
+ */
+#define ORC_CONSTANT_INIT_U8(u8) { .type = ORC_CONST_SPLAT_B, .v[0].x8[0] = u8 }
+#define ORC_CONSTANT_INIT_U16(u16) { .type = ORC_CONST_SPLAT_W, .v[0].x4[0] = u16 }
+#define ORC_CONSTANT_INIT_U32(u32) { .type = ORC_CONST_SPLAT_L, .v[0].x2[0] = u32 }
+#define ORC_CONSTANT_INIT_U64(u64) { .type = ORC_CONST_SPLAT_Q, .v[0].i = u64 }
+#define ORC_CONSTANT_INIT_U128(a, b) { .type = ORC_CONST_SPLAT_DQ, .v[0].i = a, .v[1].i = b }
+#define ORC_CONSTANT_INIT_U256(a, b, c, d) { .type = ORC_CONST_SPLAT_QQ, .v[0].i = a, .v[1].i = b, .v[2].i = c, .v[3].i = d }
+#define ORC_CONSTANT_INIT_U512(a, b, c, d, e, f, g, h) { .type = ORC_CONST_FULL, .v[0].i = a, .v[1].i = b, .v[2].i = c, .v[3].i = d, .v[4].i = e, .v[5].i = f,  .v[6].i = g, .v[7].i = h }
+
+#endif
 
 ORC_END_DECLS
 
 #endif
-
